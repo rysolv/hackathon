1648c6b0e1555aefc76afbe4040500945325ed47
Nikolai H <nikolai.sebastian@gmail.com>
2022-04-02 15:50:28 -0400

    Finishing touches

--- submissions/terrible_ui_1/worst_zodiac_picker/script.js
+++ submissions/terrible_ui_1/worst_zodiac_picker/script.js
@@ -1,13 +1,5 @@
 
-/**
- * 
- * @param {string} month1 starting month of zodiac
- * @param {number} date1 starting day of zodiac
- * @param {string} month2 ending month
- * @param {number} date2 ending day
- * @param {number} breakpoint number of days into the timespan after which it switches from month1 to month2
- * @returns {object} 
- */
+// a zodiac sign's starting month & date, ending month & date, and the day after which it switches from month1 to month2
 function createZodiac (month1, date1, month2, date2, breakpoint) {
     return {
         month1: month1,
@@ -30,7 +22,7 @@ const zodiacSigns = {
     sagittarius: createZodiac('November', 22, 'December', 21, 9),
     capricorn: createZodiac('December', 22, 'January', 19, 11),
     aquarius: createZodiac('January', 20, 'February', 18, 12),
-    pisces: createZodiac('February', 19, 'March', 20, 11) // assumes 29 days
+    pisces: createZodiac('February', 19, 'March', 20, 10) // assumes 28 days
 }
 
 let chosenZodiac;
@@ -42,6 +34,7 @@ zodiacIcons.forEach((icon) => {
     icon.addEventListener('click', () => {
         displayDayPicker();
         chosenZodiac = icon.id;
+        alertChosenZodiac();
     })
 })
 
@@ -51,13 +44,8 @@ zodiacIcons.forEach((icon) => {
         let randomG = Math.floor(Math.random()*256);
         let randomB = Math.floor(Math.random()*256);
         let random = `rgb(${randomR}, ${randomG}, ${randomB})`;
-        icon.style.backgroundColor = random; // continues flashing new colors as you move your mouse over it - hideous
+        icon.style.backgroundColor = random;
     })
-})
-
-const revealDatesBtn = document.getElementById('reveal-dates-btn');
-revealDatesBtn.addEventListener('click', () => {
-    alert('You are not worthy');
 })
 
 // Day Picker
@@ -66,8 +54,8 @@ const dayContainer = document.getElementById('day-container')
 const timePicker = document.getElementById('time-picker');
 let chosenDay;
 
-// find way to adjust depending on month?
-for (let i = 1; i <= 31; i++) {
+// all zodiacs have 30 days; overflow is determined by breakpoint
+for (let i = 1; i <= 30; i++) {
     let newDay = document.createElement('div');
     newDay.className = 'day-box';
     newDay.innerText = i;
@@ -85,7 +73,6 @@ function displayDayPicker() {
 
 function setDay(number) {
     chosenDay = Number(number);
-    console.log(chosenDay);
 }
 
 // Time Picker
@@ -99,6 +86,7 @@ const results = document.getElementById('results');
 let earliestTime = '00:01';
 let latestTime = '24:00';
 let chosenTime;
+let clickCount = 0;
 
 function setEarliestTime(time) {
     earliestTime = randomTime.innerText;
@@ -111,11 +99,19 @@ function setLatestTime(time) {
 earlyBtn.addEventListener('click', () => {
     setEarliestTime(randomTime.innerText);
     generateRandomTime();
+    clickCount++;
+    if (clickCount > 5) {
+        alert('The zodiac is displeased by your specificity.');
+    }
 })
 
 lateBtn.addEventListener('click', () => {
     setLatestTime(randomTime.innerText);
     generateRandomTime();
+    clickCount++;
+    if (clickCount > 5) {
+        alert('You\'re awfully fussy for a mortal.');
+    }
 })
 
 approveBtn.addEventListener('click', () => {
@@ -126,13 +122,13 @@ approveBtn.addEventListener('click', () => {
 
 function generateRandomTime() {
     const minHour = Number(earliestTime.substring(0, 2));
-    const maxHour = Number(latestTime.substring(0, 2)) + 1;
+    const maxHour = Number(latestTime.substring(0, 2)) +1;
     let hour = Math.floor(Math.random()* (maxHour - minHour) + minHour);
     
     let minMinute = 0;
     let maxMinute = 60;
     if (hour == Number(earliestTime.substring(0, 2))) {
-        minMinute = Number(earliestTime.substring(3)) + 1;
+        minMinute = Number(earliestTime.substring(3));
     }
     if (hour == Number(latestTime.substring(0, 2))) {
         maxMinute = Number(latestTime.substring(3)); // would be -1, but have to +1 for Math.random anyway
@@ -150,6 +146,7 @@ function generateRandomTime() {
     }
 
     randomTime.innerText = `${hour}:${minutes}`;
+    console.log('the random time is ' + randomTime.innerText + ' and the earliest is ' + earliestTime + ' and the latest is ' + latestTime);
 }
 
 // Final result
@@ -172,6 +169,32 @@ function populateResult() {
     finalDateTime.innerText = `${parseZodiacDate()} ${chosenTime}`;
 }
 
+// Popups
+
+const revealDatesBtn = document.getElementById('reveal-dates-btn');
+revealDatesBtn.addEventListener('click', () => {
+    const alertDiv = document.createElement('div');
+    alertDiv.className = 'alert-div';
+    alertDiv.innerText = 'If you do not understand the nuances of the zodiac, you are not worthy of using this site.';
+    const accept = document.createElement('button');
+    accept.innerText = 'I apologize for my foolishness';
+    accept.style.display = 'block';
+    revealDatesBtn.parentElement.appendChild(alertDiv);
+    alertDiv.appendChild(accept);
+    accept.addEventListener('click', () => {
+        alertDiv.parentElement.removeChild(alertDiv);
+    })
+})
+
+const zodiacAlert = document.getElementById('zodiac-alert');
+
+function alertChosenZodiac() {
+    const chosenIcon = document.getElementById(chosenZodiac);
+    chosenIcon.className = 'alerted-icon';
+    zodiacAlert.innerText = `You have chosen ${chosenZodiac.toUpperCase()}. There is no turning back.`;
+    zodiacAlert.classList.remove('hidden');
+}
+
 const clearAll = document.getElementById('clear-all');
 
 clearAll.addEventListener('click', () => {
@@ -180,7 +203,13 @@ clearAll.addEventListener('click', () => {
     chosenDay = null;
     earliestTime = '00:01';
     latestTime = '24:00';
+    clickCount = 0;
     timePicker.classList += 'hidden';
     dayPicker.classList += 'hidden';
     results.classList += 'hidden';
+    const images = document.querySelectorAll('#zodiac-signs img');
+    images.forEach((img) => {
+        img.classList.remove('alerted-icon');
+    })
+    zodiacAlert.classList += 'hidden';
 })
